5a46e896f33153f8530573a445b49ad63f9322ae
Jon Mueller <jonmueller@ubuntu.(none)>
2013-12-10 00:18:20 -0600

    added user sorting

--- profile.php
+++ profile.php
@@ -39,22 +39,69 @@
 			<table class="table table-striped table-hover">
 				<thead>
 					<tr>
-						<th>Name</th><th>Email</th><th>Phone</th>
+						<th>Name</th><th>Email</th><th>Phone</th><th>Relative Match</th>
 					</tr>
 				</thead>
 
 				<tbody>
 					<?php
-						$similar_people = mysqli_query($connection, "SELECT * FROM Student")
+						$user_activities = mysqli_query($connection, "SELECT * FROM Student INNER JOIN Does
+																	 ON Student.email=Does.email
+																	 WHERE Student.email='$email'")
 						or die(mysql_error());
-						// store the record of the "tblstudent" table into $row
-
-						while($row = mysqli_fetch_array($similar_people)){
-							// Print out the contents of the entry
-							echo '<tr class="tableRow">';
-							echo '<td>'.$row['name'].'</td>';
-							echo '<td class="targetEmail">'.$row['email'].'</td>';
-							echo '<td>'.$row['phoneNumber'].'</td></tr>';
+						$similar_users = array();
+						while ($row = mysqli_fetch_array($user_activities)) {
+							$query = "SELECT Student.email FROM Student INNER JOIN Does
+									  ON Student.email=Does.email
+									  WHERE idActivity=".$row['idActivity']." AND Student.email != '$email'";
+							$matched_users = mysqli_query($connection, $query)
+							or die(mysql_error());
+							while ($row1 = mysqli_fetch_array($matched_users)) {
+								if ($similar_users[$row1['email']] == null) {
+									$similar_users[$row1['email']] = 1;
+								} else {
+									$similar_users[$row1['email']] = $similar_users[$row1['email']] + 1;
+								}
+							}
+							
+							
+						}
+						arsort($similar_users);
+						$most_activities_matched;
+						$idx = 0;
+						foreach ($similar_users as $key=>$value) {
+							$user_data = mysqli_query($connection, "SELECT * FROM Student WHERE Student.email='$key'")
+										 or die(mysql_error());
+							while ($row = mysqli_fetch_array($user_data)) {
+							
+								if ($idx == 0) {
+									$most_activities_matched = $value;
+									$idx = 1;
+								}
+								$percent_match = $value / $most_activities_matched * 100;
+							
+								echo '<tr class="tableRow">';
+								echo '<td>'.$row['name'].'</td>';
+								echo '<td class="targetEmail">'.$row['email'].'</td>';
+								echo '<td>'.$row['phoneNumber'].'</td>';
+								
+								echo '<td><div class="progress">';
+								if ($percent_match > 70) {
+									$bar = '<div class="progress-bar progress-bar-success"';
+								} else if ($percent_match > 30) {
+									$bar = '<div class="progress-bar progress-bar-warning"';
+								} else {
+									$bar = '<div class="progress-bar progress-bar-danger"';
+								}
+								echo $bar;
+								echo 'role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '.$percent_match.'%;"><span style="color: #000000">';
+								//echo $percent_match.'%';
+								echo '</span></div></div></td><td>'.$percent_match.'%</td></tr>';
+								//echo '<td>'.$percent_match.'</td></tr>';
+							}
+							
+							
+					
 						}
 
 					?>
